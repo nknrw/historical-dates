bf2830018be9ed75630d956b5a8b1f7754c9c16c
nknrw <nknrw@yandex.ru>
2023-09-12 18:10:15 +0300

    fix: reformat code

---
+++ src/components/EventList/EventList.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {FreeMode, Navigation, Pagination} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/scss/pagination';
+import {Event as EventType} from '../../types';
+import Event from '../EventElement/EventElement';
+import './EventList.scss';
+
+interface EventListProps {
+  currentPage: number;
+  eventsPerPage: number;
+  events: EventType[];
+}
+
+const EventList: React.FC<EventListProps> = ({
+                                               currentPage,
+                                               eventsPerPage,
+                                               events,
+                                             }) => {
+  return (
+    <div className='event-list'>
+      <Swiper
+        key={currentPage}
+        breakpoints={{
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 80,
+            freeMode: true,
+            grabCursor: true,
+            pagination: false,
+            slidesOffsetAfter: 0,
+          },
+        }}
+        modules={[FreeMode, Navigation, Pagination]}
+        navigation={{
+          prevEl: '.prev',
+          nextEl: '.next',
+        }}
+        spaceBetween={25}
+        slidesPerView={1.75}
+        pagination={true}
+        slidesOffsetAfter={130}
+      >
+        {events.map((event) => (
+          <SwiperSlide key={event.id}>
+            <Event key={event.id} event={event}/>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className='event-list-button prev'></div>
+      <div className='event-list-button next'></div>
+    </div>
+  );
+};
+
+export default EventList;
--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,18 @@
-import React from 'react';
 import './mock.json';
 
 export interface Event {
-	id: number;
-	year: number;
-	event: string;
+  id: number;
+  year: number;
+  event: string;
 }
 
 export interface CircleContainerProps {
-	totalPages: number;
-	currentPage: number;
-	onPageChange: (page: number) => void;
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
 }
 
 export interface YearRangeProps {
-	minYear: number;
-	maxYear: number;
+  minYear: number;
+  maxYear: number;
 }
